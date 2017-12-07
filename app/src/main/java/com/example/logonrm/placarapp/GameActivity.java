0ad26ab765ada05ded7dae43e44dab4b7e2af079
logonrm <logonrm@fiap.com.br>
2017-12-06 22:04:46 -0200

    Criacao da tela placar

--- app/src/main/java/com/example/logonrm/placarapp/GameActivity.java
+++ app/src/main/java/com/example/logonrm/placarapp/GameActivity.java
@@ -1,9 +1,12 @@
 package com.example.logonrm.placarapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.logonrm.placarapp.R.id.etTimeCasa;
 
@@ -15,6 +18,12 @@ public class GameActivity extends AppCompatActivity {
     private TextView tvTimeCasa;
     private TextView tvTimeVisitante;
 
+    private TextView tvPlacarTC;
+    private TextView tvPlacarTV;
+
+    private int placarTC;
+    private int placarTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,8 @@ public class GameActivity extends AppCompatActivity {
 
         tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
         tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitante);
+        tvPlacarTC = (TextView) findViewById(R.id.tvPlacarTC);
+        tvPlacarTV = (TextView) findViewById(R.id.tvPlacarTV);
 
         if(getIntent() != null){
             timeCasa = getIntent().getStringExtra("TIMECASA");
@@ -29,8 +40,47 @@ public class GameActivity extends AppCompatActivity {
 
             tvTimeCasa.setText(timeCasa);
             tvTimeVisitante.setText(timeVisitante);
-
         }
 
+        if(savedInstanceState != null){
+            placarTC = savedInstanceState.getInt("PLACARCASA");
+            placarTV = savedInstanceState.getInt("PLACARVISITANTE");
+        }
+
+        tvPlacarTC.setText(String.valueOf(placarTC));
+        tvPlacarTV.setText(String.valueOf(placarTV));
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("PLACARCASA", placarTC);
+        outState.putInt("PLACARVISITANTE", placarTV);
+    }
+
+    public void pontuarTC(View v){
+
+        placarTC = Integer.parseInt(tvPlacarTC.getText().toString());
+        placarTC += 1;
+        tvPlacarTC.setText(String.valueOf(placarTC));
+
+    }
+
+    public void pontuarTV(View v){
+
+        placarTV = Integer.parseInt(tvPlacarTV.getText().toString());
+        placarTV += 1;
+        tvPlacarTV.setText(String.valueOf(placarTV));
+
+    }
+
+    public void limpar(View v){
+
+        placarTC = 0;
+        placarTV = 0;
+        tvPlacarTC.setText(String.valueOf(placarTC));
+        tvPlacarTV.setText(String.valueOf(placarTV));
+
+    }
+
 }
